02b176d4a1b14a2dfd9d31f83076b3de4fb91563
takashi1100 <73459160+takashi1100@users.noreply.github.com>
2020-10-26 21:40:28 +0900

    add webscraper

---
+++ webscraper/webscraper.py
@@ -0,0 +1,24 @@
+import ssl
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+        parser = "html.parser"
+        sp = BeautifulSoup(html, parser)
+        for tag in sp.find_all("a"):
+            url = tag.get("href")
+            #print(url)
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
+
+#news = "https://news.google.com"
+news = "http://www.yahoo.co.jp"
+Scraper(news).scrape()
